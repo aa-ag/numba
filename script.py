8e23d5cacc8420b1bdfb07e9b6a5af7c4d43ecc7
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-08 17:57:59 -0600

    added vanilla python version of an example function that runs slowly

--- script.py
+++ script.py
@@ -1,13 +1,23 @@
 ############------------ IMPORTS ------------############
 from time import time, sleep
-
-
-############------------ GLOBAL VARIABLE(S) ------------############
+from numba import jit
 
 
 ############------------ FUNCTION(S) ------------############
+def example_function():
+    expected_result = list()
+
+    for x in range(100):
+        for y in range(1000):
+            for z in range(10000):
+                if (x + y + z) / 10 == x:
+                    expected_result.append(x)
+        print(x)
+    return expected_result    
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    start_time = time()
+    example_function()
+    print(f"took: {round(time() - start_time, 2)} seconds")
