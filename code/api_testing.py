0dddd69967e176fdfd938039f216c0375ed67805
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-09 16:25:49 -0600

    renamed secrets.py to settings.py and updated the import, added logic to hit count-tickets endpoint, getting a http 200 back

--- code/api_testing.py
+++ code/api_testing.py
@@ -2,19 +2,27 @@
 import requests
 from time import time, sleep
 from numba.core.decorators import njit
-from .secrets import token, email
+from settings import token, email
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 zd_token = token
 zd_email_address = email
-instance_url = 'https://z3n-platformdev-aaron.zendesk.com/'
+instance_url = 'https://z3n-platformdev-aaron.zendesk.com'
 
 
 ############------------ FUNCTION(S) ------------############
 def count_tickets():
-    count_tickets_endpoint = '/api/v2/tickets/count'
-    get_headers = {''}
+    count_tickets_endpoint = instance_url + '/api/v2/tickets/count'
+    get_headers = {'Accept': 'application/json'}
+
+    response = requests.get(
+                        count_tickets_endpoint,
+                        auth=(zd_email_address+'/token', zd_token),
+                        headers=get_headers
+                    )
+
+    print(response.status_code)
 
 
 
