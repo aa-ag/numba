0753e2b9618895c70e7bdfacb3ba6ae988ead574
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-09 06:53:21 -0600

    added secrets import, imported the token and added an empty create_one_test_ticket function

--- code/api_testing.py
+++ code/api_testing.py
@@ -1,14 +1,18 @@
 ############------------ IMPORTS ------------############
 from time import time, sleep
 from numba.core.decorators import njit
+import secrets
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+api_credential = secrets.token
 
 
 ############------------ FUNCTION(S) ------------############
+def create_one_test_ticket():
+    pass
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    create_one_test_ticket()
