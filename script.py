d43dea00decffb0301bfffe181aa22a00d812f72
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-08 18:01:42 -0600

    added compiled version, tested it and it did in fact run ~100 times faster, added exact count in docstring

--- script.py
+++ script.py
@@ -21,8 +21,25 @@ def example_function():
     return expected_result
 
 
+@njit
+def compiled_function():
+    '''
+     took: 1.24 seconds
+    '''
+    expected_result = list()
+
+    for x in range(100):
+        for y in range(1000):
+            for z in range(10000):
+                if (x + y + z) / 10 == x:
+                    expected_result.append(x)
+        print(x)
+    return expected_result  
+
+
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     start_time = time()
-    example_function()
+    # example_function()
+    compiled_function()
     print(f"took: {round(time() - start_time, 2)} seconds")
