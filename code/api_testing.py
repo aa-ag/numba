a44fb5a6572755f7ac2086c91e04ea3df0288517
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-09 08:57:47 -0600

    imported email and created a global variable that contains it

--- code/api_testing.py
+++ code/api_testing.py
@@ -1,19 +1,21 @@
 ############------------ IMPORTS ------------############
 from time import time, sleep
 from numba.core.decorators import njit
-from .secrets import token
+from .secrets import token, email
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 zd_token = token
+zd_email_address = email
 instance_url = 'https://z3n-platformdev-aaron.zendesk.com/'
 
 
 ############------------ FUNCTION(S) ------------############
-def create_one_test_ticket():
-    pass
+def count_tickets():
+    count_tickets_endpoint = '/api/v2/tickets/count'
+
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    create_one_test_ticket()
+    count_tickets()
