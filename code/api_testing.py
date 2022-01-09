b99e8cc67b7dafd5db78b3a16e7418d78df7842f
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-09 09:01:03 -0600

    added requests impot

--- code/api_testing.py
+++ code/api_testing.py
@@ -1,4 +1,5 @@
 ############------------ IMPORTS ------------############
+import requests
 from time import time, sleep
 from numba.core.decorators import njit
 from .secrets import token, email
@@ -13,6 +14,7 @@ instance_url = 'https://z3n-platformdev-aaron.zendesk.com/'
 ############------------ FUNCTION(S) ------------############
 def count_tickets():
     count_tickets_endpoint = '/api/v2/tickets/count'
+    get_headers = {''}
 
 
 
