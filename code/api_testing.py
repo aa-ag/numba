726bb09c291bfce8e7e11fe4a63cf4733e84d71f
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-10 17:22:14 -0600

    separated manipulate_tickets from get_tickets to test if numba could work at the transformation stage of api work

--- code/api_testing.py
+++ code/api_testing.py
@@ -1,8 +1,11 @@
 ############------------ IMPORTS ------------############
+from re import sub
+from sys import pycache_prefix
 import requests
 from time import time, sleep
 from numba.core.decorators import njit
 from settings import token, email
+from pprint import pprint
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
@@ -12,31 +15,40 @@ instance_url = 'https://z3n-platformdev-aaron.zendesk.com'
 
 
 ############------------ FUNCTION(S) ------------############
-def count_tickets():
-    count_tickets_endpoint = instance_url + '/api/v2/tickets/count'
+def get_tickets():
+    get_tickets_endpoint = instance_url + '/api/v2/tickets'
     get_headers = {'Accept': 'application/json'}
+    tickets = {'tickets': []}
+    while get_tickets_endpoint:
+        print(get_tickets_endpoint)
+        response = requests.get(
+                            get_tickets_endpoint,
+                            auth=(zd_email_address+'/token', zd_token),
+                            headers=get_headers
+                        )
 
-    response = requests.get(
-                        count_tickets_endpoint,
-                        auth=(zd_email_address+'/token', zd_token),
-                        headers=get_headers
-                    )
+        if response.status_code == 429:
+            sleep(int(response.headers['retry-after']))
 
-    if response.status_code == 429:
-        sleep(int(response.headers['retry-after']))
+        if response.status_code != 200:
+            print(f'Status: {response.status_code}\n')
+            print(response.text)
+            exit()
 
-    if response.status_code != 200:
-        print(f'Status: {response.status_code}\n')
-        print(response.text)
-        exit()
+        data = response.json()
+        tickets['tickets'].append(data['tickets'])
+        get_tickets_endpoint = data["next_page"]
 
-    ticket_count = response.json()['count']['value']
-    print(ticket_count)
+    return tickets
 
+
+def manipulate_tickets(tickets):
+    pprint(tickets)
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
+    tickets = get_tickets()
     start_time = time()
-    count_tickets()
-    print(f"took: {round(time() - start_time, 2)} seconds")
+    manipulate_tickets(tickets)
+    print(f"took: {time() - start_time} seconds")
